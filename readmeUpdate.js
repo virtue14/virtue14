7c19686b8f7a0c835c7c2377af78588db717aa54
악덕 <rudejrqkr97@gmail.com>
2024-07-19 13:17:19 +0900

    feat: 최신 블로그 포스트 아이콘 추가
    - README.md에 최신 블로그 포스트 아이콘을 추가
    - 최신 5개의 글의 제목과 링크를 아이콘과 함께 표시

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -18,7 +18,7 @@ const parser = new Parser({
     const feed = await parser.parseURL("https://virtue14.tistory.com/rss");
 
     // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
-    let latestPosts = "#### Latest Blog Posts\n\n";
+    let latestPosts = "### <img src=\"https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1605926847/noticon/ku5wj788ubjwba7pecrw.png\" width=\"30\" /> Latest Blog Posts\n\n";
     for (let i = 0; i < 5 && i < feed.items.length; i++) {
         let { title, link } = feed.items[i];
         link = link.startsWith('http://') ? 'https://' + link.slice(7) : link;
@@ -26,9 +26,9 @@ const parser = new Parser({
     }
 
     // 기존 README.md에 최신 블로그 포스트 추가
-    const newReadmeContent = readmeContent.includes("#### Latest Blog Posts")
+    const newReadmeContent = readmeContent.includes("### <img src=\"https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1605926847/noticon/ku5wj788ubjwba7pecrw.png\" width=\"30\" /> Latest Blog Posts")
         ? readmeContent.replace(
-            /#### Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
+            /### <img src="https:\/\/noticon-static\.tammolo\.com\/dgggcrkxq\/image\/upload\/v1605926847\/noticon\/ku5wj788ubjwba7pecrw\.png" width="30" \/> Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
             latestPosts
         )
         : readmeContent + latestPosts;
